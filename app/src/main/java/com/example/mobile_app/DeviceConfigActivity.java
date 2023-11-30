3e9c239da4501541760e0d963bfcf944a4dc4baf
Rajin Nagpal <rnagpal02@outlook.com>
2023-11-30 14:44:21 -0500

    Add opponent types

--- app/src/main/java/com/example/mobile_app/DeviceConfigActivity.java
+++ app/src/main/java/com/example/mobile_app/DeviceConfigActivity.java
@@ -1,7 +1,6 @@
 package com.example.mobile_app;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -31,10 +30,10 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,7 +45,6 @@ import net.openid.appauth.AuthorizationServiceConfiguration;
 import net.openid.appauth.ResponseTypeValues;
 import net.openid.appauth.TokenResponse;
 
-import java.lang.reflect.Array;
 import java.util.UUID;
 
 public class DeviceConfigActivity extends AppCompatActivity {
@@ -69,8 +67,12 @@ public class DeviceConfigActivity extends AppCompatActivity {
     // Game config service UUIDs
     public static final UUID GAME_SERVICE_UUID = new UUID(0x00, 0x03);
     public static final UUID TIME_CONTROL_CHAR_UUID = new UUID(0x00, 0x01);
+    public static final UUID OPPONENT_TYPE_CHAR_UUID = new UUID(0x00, 0x02);
+    public static final UUID OPPONENT_USERNAME_CHAR_UUID = new UUID(0x00, 0x03);
     private BluetoothGattService gameService = new BluetoothGattService(GAME_SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY);
     private BluetoothGattCharacteristic timeControlChar = new BluetoothGattCharacteristic(TIME_CONTROL_CHAR_UUID, BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_WRITE);
+    private BluetoothGattCharacteristic opponentTypeChar = new BluetoothGattCharacteristic(OPPONENT_TYPE_CHAR_UUID, BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_WRITE);
+    private BluetoothGattCharacteristic opponentUsernameChar = new BluetoothGattCharacteristic(OPPONENT_USERNAME_CHAR_UUID, BluetoothGattCharacteristic.PROPERTY_WRITE, BluetoothGattCharacteristic.PERMISSION_WRITE);
 
     // BLE
     private BluetoothManager btManager;
@@ -82,6 +84,7 @@ public class DeviceConfigActivity extends AppCompatActivity {
     // Text input
     private EditText ssidInput;
     private EditText pwInput;
+    private static EditText opponentUsernameInput;
 
     // Buttons
     private Button wifiConnectButton;
@@ -91,10 +94,15 @@ public class DeviceConfigActivity extends AppCompatActivity {
     private static TextView wifiConnectStatus;
     private static AutoCompleteTextView timeControl;
 
+    // Radio group
+    private static RadioGroup opponentType;
+
     // UI elements
     private static String ssid = "";
     private static boolean isConnected = false;
     private static int timeControlIndex = 2;
+    private static int opponentTypeIndex = 0;
+    private static String opponentUsername = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,10 +115,15 @@ public class DeviceConfigActivity extends AppCompatActivity {
         wifiConnectStatus = findViewById(R.id.wifiConnectStatus);
         lichessLoginButton = findViewById(R.id.lichessLoginButton);
         timeControl = findViewById(R.id.timeControlSelect);
+        opponentType = findViewById(R.id.opponentType);
+        opponentUsernameInput = findViewById(R.id.opponentUsername);
 
         wifiConnectButton.setOnClickListener(connectWifiListener);
         lichessLoginButton.setOnClickListener(loginLichessListener);
         timeControl.setOnItemClickListener(timeControlClickListener);
+        opponentType.setOnCheckedChangeListener(opponentTypeChangedListener);
+        opponentUsernameInput.setOnFocusChangeListener(opponentUsernameListener);
+        uiHandler.sendMessage(new Message());
 
         btManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         btAdapter = btManager.getAdapter();
@@ -133,12 +146,13 @@ public class DeviceConfigActivity extends AppCompatActivity {
         wifiService.addCharacteristic(connectedChar);
         lichessService.addCharacteristic(bearerTokenChar);
         gameService.addCharacteristic(timeControlChar);
+        gameService.addCharacteristic(opponentTypeChar);
+        gameService.addCharacteristic(opponentUsernameChar);
 
         // Rest of the services are added in callback
         bleServer.addService(wifiService);
 
         bleScanner.startScan(bleScanCallback);
-        uiHandler.sendMessage(new Message());
     }
 
     private static final Handler uiHandler = new Handler(Looper.getMainLooper()) {
@@ -158,7 +172,18 @@ public class DeviceConfigActivity extends AppCompatActivity {
                 wifiConnectStatus.setTextColor(Color.RED);
             }
 
+            Log.e("Donkey", "" + timeControlIndex);
             timeControl.setText(timeControl.getAdapter().getItem(timeControlIndex).toString(), false);
+
+            if (opponentTypeIndex == 0) {
+                opponentUsernameInput.setEnabled(false);
+                opponentType.check(R.id.radioRandom);
+            }
+            else if (opponentTypeIndex == 1) {
+                opponentUsernameInput.setEnabled(true);
+                opponentType.check(R.id.radioSpecific);
+            }
+            opponentUsernameInput.setText(opponentUsername);
             super.handleMessage(msg);
         }
     };
@@ -191,7 +216,9 @@ public class DeviceConfigActivity extends AppCompatActivity {
                                     return;
                                 }
                                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                                    bleServer.notifyCharacteristicChanged(bleDevice, bearerTokenChar, false, bearerToken.getBytes());
+                                    if (bleDevice != null) {
+                                        bleServer.notifyCharacteristicChanged(bleDevice, bearerTokenChar, false, bearerToken.getBytes());
+                                    }
                                 }
                             } else {
                                 Toast.makeText(getApplicationContext(), "Failed to authenticate", Toast.LENGTH_SHORT).show();
@@ -217,8 +244,10 @@ public class DeviceConfigActivity extends AppCompatActivity {
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ssid = ssidInput.getText().toString();
-            bleServer.notifyCharacteristicChanged(bleDevice, ssidChar, false, ssid.getBytes());
-            bleServer.notifyCharacteristicChanged(bleDevice, pwChar, false, pwInput.getText().toString().getBytes());
+            if (bleDevice != null) {
+                bleServer.notifyCharacteristicChanged(bleDevice, ssidChar, false, ssid.getBytes());
+                bleServer.notifyCharacteristicChanged(bleDevice, pwChar, false, pwInput.getText().toString().getBytes());
+            }
         }
     };
 
@@ -234,7 +263,6 @@ public class DeviceConfigActivity extends AppCompatActivity {
                         "2", // the client ID, typically pre-registered and static
                         ResponseTypeValues.CODE, // the response_type value: we want a code
                         Uri.parse("com.example.mobileapp:/oauth2redirect")); // the redirect URI to which the auth response is sent
-
 
         AuthorizationRequest authRequest = authRequestBuilder
                 .setScope("email:read preference:read challenge:read challenge:write challenge:bulk board:play bot:play")
@@ -255,7 +283,43 @@ public class DeviceConfigActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
             byte[] data = { (byte)(i) };
-            bleServer.notifyCharacteristicChanged(bleDevice, timeControlChar, false, data);
+            timeControlIndex = i;
+            if (bleDevice != null) {
+                bleServer.notifyCharacteristicChanged(bleDevice, timeControlChar, false, data);
+            }
+        }
+    };
+
+    private final RadioGroup.OnCheckedChangeListener opponentTypeChangedListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            if (radioGroup.getCheckedRadioButtonId() == R.id.radioRandom) {
+                opponentTypeIndex = 0;
+            }
+            else if (radioGroup.getCheckedRadioButtonId() == R.id.radioSpecific) {
+                opponentTypeIndex = 1;
+            }
+
+            uiHandler.sendMessage(new Message());
+            byte[] data = { (byte)(opponentTypeIndex)};
+
+            if (bleDevice != null) {
+                bleServer.notifyCharacteristicChanged(bleDevice, opponentTypeChar, false, data);
+            }
+
+            if (opponentTypeIndex == 1 && bleDevice != null) {
+                bleServer.notifyCharacteristicChanged(bleDevice, opponentUsernameChar, false, opponentUsernameInput.getText().toString().getBytes());
+            }
+        }
+    };
+
+    private View.OnFocusChangeListener opponentUsernameListener = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View view, boolean b) {
+            if (!b) {
+                opponentUsername = opponentUsernameInput.getText().toString();
+                bleServer.notifyCharacteristicChanged(bleDevice, opponentUsernameChar, false, opponentUsername.getBytes());
+            }
         }
     };
 
@@ -368,6 +432,15 @@ public class DeviceConfigActivity extends AppCompatActivity {
             }
             else if (characteristic == connectedChar) {
                 isConnected = (value[0] != 0);
+            }
+            else if (characteristic == timeControlChar) {
+                timeControlIndex = value[0];
+            }
+            else if (characteristic == opponentTypeChar) {
+                opponentTypeIndex = value[0];
+            }
+            else if (characteristic == opponentUsernameChar) {
+                opponentUsername = value[0] == 0 ? "" : value.toString();
             }
 
             uiHandler.sendMessage(new Message());
